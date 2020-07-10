1fb29dfb52cfa9c626c455b3815c79398438a8b1
Christian Rebischke <chris@shibumi.dev>
2020-07-10 04:17:15 +0200

    calculate distance via geopy library

---
+++ msg.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from geopy.distance import distance
+
+class MSG:
+
+    def __init__(self):
+        self.distances = []
+        positions = {}
+        with open("msg_standorte_deutschland.csv") as csvFile:
+            # skip first line
+            next(csvFile)
+            for line in csvFile:
+                # remove newline from every line and split at delimeter ";"
+                l = line.rsplit("\n")[0].split(",")
+                positions[l[0]] = (l[6], l[7])
+        for source, source_coord in positions.items():
+             for dest, dest_coord in positions.items():
+                # calculate distance between each position via geopy library
+                # we could also calculate this by ourself via: https://en.wikipedia.org/wiki/Vincenty's_formulae
+                # or via: https://en.wikipedia.org/wiki/Haversine_formula
+                # the geopy library uses the Vincenty's formulae, because it's more precise.
+                self.distances.append([source, dest, distance(source_coord, dest_coord).m])
+        # sort based on distance == third element in calculated distances list
+        self.distances.sort(key=lambda x: x[2])
+        # remove all distances with distance 0.0, because these are source == destination
+        self.distances = list(filter(lambda x: x[2] != 0.0, self.distances))
+        # from now here on use dijkstar
+        print(self.distances)
+
+
+
+
+
+if __name__ == "__main__":
+    MSG()     
